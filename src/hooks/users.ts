7c3a34d2eaf7067cfc4efb8839e30ecc86763c78
vadimition <vadym.sirenko.triare@gmail.com>
2022-08-25 15:57:18 +0300

    feat: user profile

--- src/hooks/users.ts
+++ src/hooks/users.ts
@@ -59,3 +59,69 @@ export const useUsersGet = (): UseUsersGet => {
 
   return { fetch, data, loading, error, clearError };
 };
+
+export interface UserId {
+  bio: null;
+  company: null;
+  email: null;
+  hireable: null;
+  location: null;
+  name: null;
+  twitter_username: null;
+  id: number;
+  followers: number;
+  following: number;
+  public_gists: number;
+  public_repos: number;
+  site_admin: false;
+  avatar_url: string;
+  blog: string;
+  created_at: string;
+  events_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  gravatar_id: string;
+  html_url: string;
+  login: string;
+  node_id: string;
+  organizations_url: string;
+  received_events_url: string;
+  repos_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  type: string;
+  updated_at: string;
+  url: string;
+}
+
+interface UseUsersGetId {
+  fetch: (name?: string) => void;
+  data: UserId | null;
+  loading: boolean;
+  error: string;
+  clearError: () => void;
+}
+
+export const useUsersGetId = ():UseUsersGetId => {
+  const [data, setData] = useState<UserId | null>(null);
+  const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetch = (name?: string) => {
+    if (name) {
+      setLoading(true);
+
+      axios.get<UserId>(`https://api.github.com/users/${name}`)
+        .then((res) => setData(res.data))
+        .catch((res) => setError(res?.data?.message || 'Something went wrong!'))
+        .finally(() => setLoading(false));
+    } else {
+      setData(null);
+    }
+  };
+
+  const clearError = () => setError('');
+
+  return { fetch, data, loading, error, clearError };
+};
