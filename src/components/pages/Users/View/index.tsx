837796518f05a1c6f78f8f0dd511dfe3765568e5
vadimition <vadym.sirenko.triare@gmail.com>
2022-08-25 10:51:44 +0300

    feat: routes

---
+++ src/components/pages/Users/View/index.tsx
@@ -0,0 +1,5 @@
+const View = ():JSX.Element => (
+  <div>View</div>
+);
+
+export default View;
---
+++ src/pages/Users/View/index.tsx
@@ -0,0 +1,11 @@
+import Content from '../../../components/pages/Users/View';
+
+const View = (): JSX.Element => {
+  document.title = 'Atlana: Profile';
+
+  return (
+    <Content />
+  );
+};
+
+export default View;
